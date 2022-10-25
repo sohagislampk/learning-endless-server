dd32171ca954c3cd6f40a9b75b45c9423ea5b605
sohagislampk <sohagislampk@gmail.com>
2022-10-25 20:46:41 +0600

    Couse Category responce added

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const categories = require('./data/course-categories.json');
+const courses = require('./data/course.json');
+
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('Course API is Running')
+})
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+});
+
+app.listen(port, () => {
+
+})
